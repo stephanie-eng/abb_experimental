cac58669e9ac3abbf2e9a56c1ef6cf48193d845b
Stephanie Eng <stephanie.eng@picknik.ai>
2022-01-28 15:11:30 -0500

    Initial ros2 port of IRB1200 5 90

---
+++ abb_irb1200_support/launch/irb1200_5_90.launch.py
@@ -0,0 +1,55 @@
+import os
+import yaml
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from launch.actions import ExecuteProcess
+from ament_index_python.packages import get_package_share_directory
+import xacro
+
+
+def load_file(package_name, file_path):
+    package_path = get_package_share_directory(package_name)
+    absolute_file_path = os.path.join(package_path, file_path)
+
+    try:
+        with open(absolute_file_path, "r") as file:
+            return file.read()
+    except EnvironmentError:  # parent of IOError, OSError *and* WindowsError where available
+        return None
+
+
+def load_yaml(package_name, file_path):
+    package_path = get_package_share_directory(package_name)
+    absolute_file_path = os.path.join(package_path, file_path)
+
+    try:
+        with open(absolute_file_path, "r") as file:
+            return yaml.safe_load(file)
+    except EnvironmentError:  # parent of IOError, OSError *and* WindowsError where available
+        return None
+
+
+def generate_launch_description():
+    # planning_context
+
+    robot_description_config = xacro.process_file(
+        os.path.join(
+            get_package_share_directory("abb_irb1200_support"),
+            "urdf",
+            "irb1200_5_90.xacro",
+        )
+    )
+    robot_description = {"robot_description": robot_description_config.toxml()}
+
+    robot_state_publisher_node = Node(
+        package="robot_state_publisher",
+        executable="robot_state_publisher",
+        output="both",
+        parameters=[robot_description],
+    )
+    joint_state_publisher_node = Node(
+        package="joint_state_publisher_gui",
+        executable="joint_state_publisher_gui"
+    )
+
+    return LaunchDescription([robot_state_publisher_node, joint_state_publisher_node])
